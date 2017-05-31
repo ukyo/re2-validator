48c7139e8f298d3d235c13f26e307f738cfff2f5
ukyo <ukyo.web@gmail.com>
2017-05-31 13:33:26 +0900

    remove comment

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #! /bin/sh
 
 emcc -Ire2 --bind -o dist/re2.js  re2_binding.cc re2/obj/libre2.a -s WASM=1 --llvm-lto 1 --llvm-opts 3 -s NO_FILESYSTEM=1  -s EXPORTED_RUNTIME_METHODS="[]" --closure 0 -Oz --pre-js pre.js --post-js post.js
-# echo "\nexport const validate = this.___re2.validate;\nexport const ready = this.___re2.ready;" >> dist/re2.js
