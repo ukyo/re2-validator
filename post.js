a460101e1b9c90349a7f098ee300c551aac48684
ukyo <ukyo.web@gmail.com>
2017-05-31 12:33:06 +0900

    small fix

--- post.js
+++ post.js
@@ -2,7 +2,7 @@ var re2_buffer = new Uint8Array(1024);
 
 module.exports['validate'] = function(s) {
   var size = stringToUTF8Array(s, re2_buffer, 0, 1024);
-  return Module['_validate'](re2_buffer);
+  return Module['_validate'](re2_buffer.subarray(0, size));
 };
 
 module.exports['ready'] = new Promise(function(resolve) {
